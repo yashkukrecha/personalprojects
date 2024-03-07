8d14ec319b6da9cfb54c6af988658734197b0117
Yash Kukrecha <ykukrecha@gmail.com>
2024-03-07 16:50:06 -0600

    updated pfp

--- personal-web/src/Pages/Home.js
+++ personal-web/src/Pages/Home.js
@@ -3,7 +3,7 @@ import '../Styles/Home.css';
 export default function Home() {
     return (
         <div id="home-container"> 
-            <img className="pfp" alt="profile-picture" src="https://media.licdn.com/dms/image/D4E03AQGnqgGZtS95cw/profile-displayphoto-shrink_200_200/0/1686077189590?e=1707955200&v=beta&t=uNx295_YssbStty9YkeB2LWRoCEO4reVzeA9KboshvQ"></img>
+            <img className="pfp" alt="profile-picture" src={require(`../Icons/linkedin.jpg`)}></img>
             <h1 id="name"> 🤘 Yash Kukrecha 🤘 </h1>
             <h3 className="unbold" id="bio"> 
                 Hi, my name is Yash Kukrecha! I'm a freshman at the University of Texas at Austin with an expected graduation date in 2027. 
