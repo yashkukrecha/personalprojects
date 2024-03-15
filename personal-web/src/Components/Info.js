0e2b1d3328dc36e8cf920ac8cdab5100780bcf32
Yash Kukrecha <ykukrecha@gmail.com>
2024-03-15 16:08:26 -0500

    added phone styling

--- personal-web/src/Components/Info.js
+++ personal-web/src/Components/Info.js
@@ -1,9 +1,10 @@
 import '../Styles/App.css';
+import '../Styles/Info.css';
 
 export default function Info(props) {
     return (
         <div className="column-container">
-            {props.index % 2 === 1 && <a className="tags" href={props.link} target="_blank"> {props.method}🔗 </a>}
+            {props.index % 2 === 1 && <a id="phonetags" className="tags" href={props.link} target="_blank"> {props.method}🔗 </a>}
             {props.index % 2 === 0 && <h2 className="desc"> {props.method}: {props.link} </h2>}
         </div>
     )
